a55e92f7eb3858bab26b8732677bb6d56a0797b5
ThompsonHen <624578239@qq.com>
2020-09-30 11:58:34 +0800

    Initial commit

---
+++ src/Leetcode0001.java
@@ -0,0 +1,44 @@
+/***
+ * 1. 两数之和
+ *
+ *给定一个整数数组 nums和一个目标值 target，请你在该数组中找出和为目标值的那两个整数，并返回他们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ *
+ * 示例:
+ *
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ *
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+import java.util.HashMap;
+
+public class Leetcode0001 {
+    public int[] twoSum(int[] nums, int target) {
+
+        if (nums.length == 0 || nums.length == 1){
+            return null;
+        }
+        HashMap<Integer, int[]> map = new HashMap<>();
+
+        for (int i = 0; i <= nums.length; i++) {
+
+            int num = nums[i];
+
+            int balance = target - num;
+
+            if (map.get(balance) == null){
+                int[] data = {balance, i};
+                map.put(num, data);
+            }else if (map.get(balance)[0] == target - num){
+                return new int[]{map.get(balance)[1] , i};
+            }
+        }
+        return null;
+    }
+}
